e7ec0e32976ad376c36948c0804d430606ba7b1a
Michael Hoesing <mhoesing@unomaha.edu>
2013-06-05 00:13:13 -0500

    Readme

--- Choose_a_Workbook_and_list_all_sheets.ps1
+++ Choose_a_Workbook_and_list_all_sheets.ps1
@@ -1,4 +1,9 @@
 # Thanks Ati for the file explorer call and save the results to a text file
+Param ( 
+    [Parameter(Mandatory=$true,Position=0)] 
+    [string]$ps1filepath,
+)
+
 Function Get-FileName()
 {   
  [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
@@ -15,7 +20,7 @@ Function Get-FileName()
 
 $initialDirectory = ".\"
 $filePath = Get-FileName
-$filePath | out-file "C:\Data\Powershell_Library\filePath.txt" -Encoding ASCII
+$filePath | out-file "$ps1filepathfilePath.txt" -Encoding ASCII
 
 #Thanks to Bryan C O'Connell for the listing worksheets cmdlets logic  http://bryanoconnell.blogspot.com/p/licenses.html
 $Excel = New-Object -ComObject "Excel.Application" 
@@ -27,7 +32,7 @@ if ($Workbook.Worksheets.Count -gt 0)
  { 
     $Worksheet = $Workbook.Worksheets.item(1) 
     foreach($Worksheet in $Workbook.Worksheets) { 
-	    $worksheetname =  $Worksheet.Name | out-file "C:\Data\Powershell_Library\worksheetlist.txt" -Encoding ASCII -append
+	    $worksheetname =  $Worksheet.Name | out-file "$ps1filepathworksheetlist.txt" -Encoding ASCII -append
          } 
 } 
 
